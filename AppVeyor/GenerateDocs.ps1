5e75cc49fb55bdfa2e04bba82b61fa57944e2358
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-09 08:49:06 -0300

    Publish to gh-pages

--- AppVeyor/GenerateDocs.ps1
+++ AppVeyor/GenerateDocs.ps1
@@ -2,10 +2,9 @@ git config --global credential.helper store
 Add-Content "$HOME\.git-credentials" "https://$($env:git_access_token):x-oauth-basic@github.com`n"
 git config --global user.email "dariokondratiuk@gmail.com"
 git config --global user.name "Darío Kondratiuk"
+git remote add pages https://github.com/kblok/docfx-playground.git
 docfx metadata docfx_project/docfx.json
 docfx build docfx_project/docfx.json -o docs
 git checkout master
 git add docfx_project/*
-git add docs/*
-git commit -m "Docs for"
-git push origin
+git subtree push --prefix docs origin pages
