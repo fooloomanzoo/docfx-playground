fca13b20d4f8490aa8150354cb8bd1d93242bd7a
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-09 08:59:42 -0300

    Maybe this?

--- AppVeyor/GenerateDocs.ps1
+++ AppVeyor/GenerateDocs.ps1
@@ -4,6 +4,8 @@ git config --global user.email "dariokondratiuk@gmail.com"
 git config --global user.name "Darío Kondratiuk"
 git remote add pages https://github.com/kblok/docfx-playground.git
 git checkout master
+$Env:VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional"
+$Env:VisualStudioVersion="15.0"
 docfx metadata docfx_project/docfx.json
 docfx build docfx_project/docfx.json -o docs
 git add docfx_project/*
