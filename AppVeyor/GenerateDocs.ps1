c6fe8a1631db3d4eb939a3663c327b7ce367b933
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-10 20:47:41 -0300

    Only tags

--- AppVeyor/GenerateDocs.ps1
+++ AppVeyor/GenerateDocs.ps1
@@ -1,14 +1,16 @@
-git config --global credential.helper store
-Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:git_access_token):x-oauth-basic@github.com`n"
+if($env:APPVEYOR_REPO_TAG -eq 'True'){
+    git config --global credential.helper store
+    Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:git_access_token):x-oauth-basic@github.com`n"
 
-git config --global user.email "dariokondratiuk@gmail.com"
-git config --global user.name "Darío Kondratiuk"
-git remote add pages https://github.com/kblok/docfx-playground.git
-git fetch pages
-git checkout master
-git subtree add --prefix docs pages/gh-pages
-docfx metadata docfx_project/docfx.json
-docfx build docfx_project/docfx.json -o docs
-git add docs/* -f
-git commit -m "Docs build $($env:APPVEYOR_BUILD_VERSION)"
-git subtree push --prefix docs pages gh-pages
+    git config --global user.email "dariokondratiuk@gmail.com"
+    git config --global user.name "Darío Kondratiuk"
+    git remote add pages https://github.com/kblok/docfx-playground.git
+    git fetch pages
+    git checkout master
+    git subtree add --prefix docs pages/gh-pages
+    docfx metadata docfx_project/docfx.json
+    docfx build docfx_project/docfx.json -o docs
+    git add docs/* -f
+    git commit -m "Docs build $($env:APPVEYOR_BUILD_VERSION)"
+    git subtree push --prefix docs pages gh-pages
+}
