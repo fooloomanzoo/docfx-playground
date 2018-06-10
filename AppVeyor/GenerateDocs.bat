bc7d01fd29640f9ed4d5794e64b47a572d2b3fd8
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-10 19:25:32 -0300

    Another attempt

--- AppVeyor/GenerateDocs.bat
+++
@@ -1,7 +0,0 @@
-git checkout master
-git subtree add --prefix docs gh-pages
-docfx metadata docfx_project/docfx.json
-docfx build docfx_project/docfx.json -o docs
-git add docs/
-git commit -m "Docs vx"
-git subtree push --prefix docs pages gh-pages
--- AppVeyor/SetupGit.ps1
+++
@@ -1,5 +0,0 @@
-git config --global credential.helper store
-Add-Content "$env:USERPROFILE.git-credentials" "https://$($env:git_access_token):x-oauth-basic@github.com`n"
-git config --global user.email "dariokondratiuk@gmail.com"
-git config --global user.name "Darío Kondratiuk"
-git remote add pages https://github.com/kblok/docfx-playground.git
