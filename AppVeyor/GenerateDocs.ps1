0f7211c8f1279330e2256b5fbbb1f8fe3d6cd02d
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-10 07:42:45 -0300

    Different add-content

--- AppVeyor/GenerateDocs.ps1
+++ AppVeyor/GenerateDocs.ps1
@@ -1,11 +1,9 @@
 git config --global credential.helper store
-Add-Content "$HOME\.git-credentials" "https://$($env:git_access_token):x-oauth-basic@github.com`n"
+Add-Content "$env:USERPROFILE.git-credentials" "https://$($env:git_access_token):x-oauth-basic@github.com`n"
 git config --global user.email "dariokondratiuk@gmail.com"
 git config --global user.name "Darío Kondratiuk"
 git remote add pages https://github.com/kblok/docfx-playground.git
 git checkout master
-$Env:VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional"
-$Env:VisualStudioVersion="15.0"
 docfx metadata docfx_project/docfx.json
 docfx build docfx_project/docfx.json -o docs
 git add docfx_project/*
