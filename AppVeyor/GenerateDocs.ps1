775336b0289fbdd6a30c88d57bef5a87cc0f8db5
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-10 20:23:21 -0300

    Fetch pages remote

--- AppVeyor/GenerateDocs.ps1
+++ AppVeyor/GenerateDocs.ps1
@@ -4,6 +4,7 @@ Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:git_access_token
 git config --global user.email "dariokondratiuk@gmail.com"
 git config --global user.name "Darío Kondratiuk"
 git remote add pages https://github.com/kblok/docfx-playground.git
+git fetch pages
 git checkout master
 git subtree add --prefix docs pages/gh-pages
 docfx metadata docfx_project/docfx.json
